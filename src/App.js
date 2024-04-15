ea6a75df48fe218a08cb6dfee310bfce378fd8a4
mujtabasaqib <mujtabasaqib99@gmail.com>
2024-04-15 17:29:15 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {marked} from 'marked';
+import DOMPurify from 'dompurify';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const introText = `
+# Welcome to My Markdown Preview
+
+This document showcases Markdown elements for HTML rendering:
+
+## Sub Heading
+
+**Bolded** text.
+
+### Link and Inline Code
+
+[Markdown Guide](https://www.markdownguide.org/) and \`inline code\`.
+
+### Code Block
+
+\`\`\`python
+def greet(name):
+    print(f"Hello, {name}!")
+\`\`\`
+
+### List Item
+
+- Item 1
+- Item 2
+- Item 3
+
+### Blockquote
+
+> Markdown is a lightweight markup language with plain-text formatting syntax.
+
+Explore Markdown syntax and its HTML output!
+`;
+
+class App extends React.Component {
+
+  constructor(props){
+
+    super(props);
+
+    this.state = {
+      userInput: introText
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({
+      userInput: event.target.value
+    })
+  }
+
+  render(){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <div id="edit-container" className='shadow'>
+        <textarea id="editor" value={this.state.userInput} 
+      onChange={this.handleChange} className=''></textarea>
+      </div>
+      <div id="preview" className='shadow'>
+        <div id="prev-text" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(marked.parse(this.state.userInput))}}/>
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
